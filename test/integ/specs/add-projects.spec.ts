57053dc28aa096c8d6ac7fc220fd7a1eceefdb7b
sabarish narain <sabarish.narain@gmail.com>
2019-10-15 16:18:34 -0500

    Refactor integration tests (#63)

--- test/integ/specs/add-projects.spec.ts
+++ test/integ/specs/add-projects.spec.ts
@@ -1,29 +1,15 @@
 import Session from '../page-objects/Session';
 import {assert} from 'chai';
-import Projects from '../page-objects/projects/Projects';
-import Project from '../page-objects/projects/Project';
-import Links from '../page-objects/common/Links';
+import * as integTestHelper from './integTestHelper';
 
 describe('Project usecases', () => {
   it('Create & Delete Project', () => {
-      const home = Session.init().loginAs();
-
-      let projects: Projects = Links.projects();
-      const PROJECT_NAME = 'tempproject';
-
-      // clean up existing projects if present
-      if (projects.isProjectExists(PROJECT_NAME)) {
-          projects = projects.openProject(PROJECT_NAME).deleteProject();
-      }
-      assert.isFalse(projects.isProjectExists(PROJECT_NAME));
-
-      projects.enterProjectName(PROJECT_NAME).create();
-      assert.isTrue(projects.isProjectExists(PROJECT_NAME));
-
-      const project: Project = projects.openProject(PROJECT_NAME);
-      project.deleteProject();
+      Session.init().loginAs();
+      const PROJECT_NAME = 'add-project-test' + Date.now();
+      const projects = integTestHelper.createProject(PROJECT_NAME);
+      projects.openProject(PROJECT_NAME).deleteProject();
       assert.isFalse(projects.isProjectExists(PROJECT_NAME));
 
      });
 
- }) 
+ });
--- test/integ/specs/rename-project.spec.ts
+++ test/integ/specs/rename-project.spec.ts
@@ -1,24 +1,14 @@
 import Session from '../page-objects/Session';
 import {assert} from 'chai';
-import Projects from '../page-objects/projects/Projects';
 import Project from '../page-objects/projects/Project';
-import Links from '../page-objects/common/Links';
+import * as integTestHelper from './integTestHelper';
 
 describe('Project usecases', () => {
   it('Create & rename Project', () => {
     Session.init().loginAs();
 
-    let projects: Projects = Links.projects();
     const PROJECT_NAME = 'tempproject' + Date.now();
-
-    // clean up existing projects if present
-    if (projects.isProjectExists(PROJECT_NAME)) {
-        projects = projects.openProject(PROJECT_NAME).deleteProject();
-    }
-    assert.isFalse(projects.isProjectExists(PROJECT_NAME));
-
-    projects.enterProjectName(PROJECT_NAME).create();
-    assert.isTrue(projects.isProjectExists(PROJECT_NAME));
+    const projects = integTestHelper.createProject(PROJECT_NAME);
 
     const project: Project = projects.openProject(PROJECT_NAME);
     const newname = 'helloworld' + Date.now();
